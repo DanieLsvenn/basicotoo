ee5dce10fa73a8bcdcaf5888d6462e10b2139900
DanieLsvenn <seizurebabe@gmail.com>
2025-06-04 12:02:55 +0700

    Initial Push

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,13 @@
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
+
+const Page = () => {
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div>Hello</div>
+      </MaxWidthWrapper>
+    </>
+  );
+};
+
+export default Page;
